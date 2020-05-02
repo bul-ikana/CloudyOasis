479789524b3bf3e11b2981cff0d8a44daf48ce96
Hugo Aguirre <hugo.agmtz@gmail.com>
2020-05-02 15:07:31 -0500

    Dissipating cloud

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cloud : MonoBehaviour
+{
+	public int water;
+	public Text score;
+    public float elapsed;
+    public ParticleSystem steam;
+
+    void Start()
+    {
+        Form();
+    }
+
+    void Update()
+    {
+    	Dissipate();
+    }
+
+    // --------------------------------------------------------- //
+
+    private void Form () {
+    	elapsed = 0f;
+        water   = 100;
+        UpdateScore();
+    }
+
+    private void Dissipate () {
+        elapsed += Time.deltaTime;
+        if (elapsed >= 3f) {
+            steam.Play();
+            elapsed = elapsed % 3f;
+            water--;
+            UpdateScore();
+        }
+    }
+
+    private void UpdateScore () {
+    	score.text = water.ToString();
+    }
+}
