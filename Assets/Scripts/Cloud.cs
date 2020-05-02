156b21a37b5654948d4a43e258083806a83fb55c
Hugo Aguirre <hugo.agmtz@gmail.com>
2020-05-02 18:05:11 -0500

    Rain particles

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class Cloud : WaterBody
 {
+    public ParticleSystem rain;
     public ParticleSystem steam;
 
     void Start()
@@ -35,6 +36,7 @@ public class Cloud : WaterBody
 
     public void Rain (int rainSize) 
     {
+        rain.Play();
         Deplete(rainSize);
     }
 }
